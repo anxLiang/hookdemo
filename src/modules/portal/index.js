3f557e663c8cf4666b796ddbb40275b389a4a880
梁才军 <liangcj@yonyou.com>
2019-11-01 08:48:14 +0800

    组件线

--- src/modules/portal/index.js
+++ src/modules/portal/index.js
@@ -1,11 +1,7 @@
 import CompPortal from "./CompPortal";
-import ProjectPortal from "./ProjectPortal";
-import CorePortal from "./CorePortal";
 
 const RENDER_MAP = {
-  project: ProjectPortal,
   component: CompPortal,
-  core: CorePortal
 };
 
 const Render = RENDER_MAP[process.env.RUN_TYPE];
